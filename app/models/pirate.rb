35443bf72f1af074b9dfc0212dd349359a0bacbb
Janis Lebron <janislebron@gmail.com>
2021-06-04 10:10:53 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,21 +1,4 @@
 class Pirate
-  attr_reader :name, :type, :booty
   
-  @@ all = []
-  
-  def initialize(name, type, booty)
-    @name = name
-    @type = type
-    @booty = booty
-    @@all << self
-  end 
-  
-  def self.all 
-    @@all 
-  end
-  
-  def self.clear
-    @@all
-  end
   
 end
