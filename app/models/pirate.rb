8af323cc3655477afba2c1fb2f1662a740eda8c9
Janis Lebron <janislebron@gmail.com>
2021-06-04 10:13:01 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,4 +1,13 @@
 class Pirate
+  attr_reader :name, :weight, :height
   
+  @@all = []
+  
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end 
   
 end
