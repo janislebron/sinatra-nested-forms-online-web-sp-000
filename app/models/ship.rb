ccc14acd561a631a75531be4847c1089e5d4051e
Janis Lebron <janislebron@gmail.com>
2021-06-04 10:55:06 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,20 +1,20 @@
 class Ship
-  attr_reader :name, :type, :booty
-  
-  SHIPS = []
-  
-  def initialize(params)
-    @name = params[:name]
-    @type = params[:type]
-    @booty = params[:booty]
-    SHIPS << self
-  end 
-  
-  def self.all 
-    SHIPS.all
-  end
-  
-  def self.clear
-    SHIPS.clear
-  end
+    attr_accessor :name, :type, :booty
+    
+    @@all = []
+
+    def initialize(params)
+      @name = params[:name]
+      @type = params[:type]
+      @booty = params[:booty]
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.clear
+      @@all.clear
+    end
 end
