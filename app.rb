11b3c9e75a8d2e524d31e5361666dabdd106e30d
Janis Lebron <janislebron@gmail.com>
2021-06-04 10:45:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ module FormsLab
       erb :'pirates/new'
     end
     
-    post '/' do
+    post '/pirates' do
        @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
